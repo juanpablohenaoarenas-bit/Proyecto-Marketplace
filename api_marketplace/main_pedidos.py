be138df5f1ca0d4ba1a3c2ba8da42e074df9c75f
Jeison Stiven Lopez Castro <jeisonlopez2599@gmail.com>
2026-09-09 17:50:00 -0500

    Se crearon otros  pipelines

--- api_marketplace/main_pedidos.py
+++ api_marketplace/main_pedidos.py
@@ -6,7 +6,11 @@ def main_pedidos():
         print("1. Crear pedido")
         print("2. Consultar pedidos")
         print("3. Consultar pedidos por comprador")
-        print("4. Salir")
+        print("4. Consultar pedidos con comprador y su información")
+        print("5. Consultar usuarios con pedidos")
+        print("6. Consultar pedidos con detalle")
+        print("7. Consultar pedidos con información completa")
+        print("8. Salir")
 
         opcion = input("Ingrese el número de la opción deseada: ")
 
@@ -17,6 +21,14 @@ def main_pedidos():
         elif opcion == "3":
             pedidos_comprador()
         elif opcion == "4":
+            pedidos_con_comprador()
+        elif opcion == "5":
+            usuarios_con_pedidos()
+        elif opcion == "6":
+            pedidos_con_detalle()
+        elif opcion == "7":
+            pedidos_con_informacion_completa()
+        elif opcion == "8":
             break
         else:
             print("Opción inválida. Por favor, seleccione una opción válida.")
--- api_marketplace/pedidos.py
+++ api_marketplace/pedidos.py
@@ -1,4 +1,4 @@
-from conexion import coleccion_pedido
+from conexion import coleccion_pedido, coleccion_usuario
 
 def crear_pedido():
     detalle = input("Ingrese el detalle del pedido: ")
@@ -41,3 +41,91 @@ def pedidos_texto():
     pedidos = coleccion_pedido.find({"detalle": {"$regex": detalle}})
     for pedido in pedidos:
         print(f"ID: {pedido['_id']}, Detalle: {pedido['detalle']}, Pago: {pedido['pago']}, Comprador: {pedido['comprador']}")   
+
+# pipeline con $lookup
+def pedidos_con_comprador():
+    pipeline = [
+        {
+            "$lookup": {
+                "from": "usuarios",
+                "localField": "comprador",
+                "foreignField": "nombre",
+                "as": "informacion_comprador"
+            }
+        }
+    ]
+    pedidos = coleccion_pedido.aggregate(pipeline)
+    for pedido in pedidos:
+        print(f"ID: {pedido['_id']}, Detalle: {pedido['detalle']}, Pago: {pedido['pago']}, Comprador: {pedido['comprador']}")
+        if pedido['informacion_comprador']:
+            comprador_info = pedido['informacion_comprador'][0]
+            print(f"Información del comprador - Nombre: {comprador_info['nombre']}, Correo: {comprador_info['correo']}, Tipo de usuario: {comprador_info['tipo_usuario']}")
+        else:
+            print("No se encontró información del comprador.")
+
+
+# pipeline con $unwind
+def usuarios_con_pedidos():
+    pipeline = [
+        {
+            "$lookup": {
+                "from": "pedidos",
+                "localField": "nombre",
+                "foreignField": "comprador",
+                "as": "pedidos_usuario"
+            }
+        },
+        {
+            "$unwind": "$pedidos_usuario"
+        }
+    ]
+    usuarios = coleccion_usuario.aggregate(pipeline)
+    for usuario in usuarios:
+        print(f"Nombre: {usuario['nombre']}, Correo: {usuario['correo']}, Tipo de usuario: {usuario['tipo_usuario']}")
+        print(f"Pedido - Detalle: {usuario['pedidos_usuario']['detalle']}, Pago: {usuario['pedidos_usuario']['pago']}")
+
+# pipeline con $project
+
+def pedidos_con_detalle():
+    pipeline = [
+        {
+            "$project": {
+                "_id": 0,
+                "detalle": 1,
+                "pago": 1,
+                "comprador": 1
+            }
+        }
+    ]
+    pedidos = coleccion_pedido.aggregate(pipeline)
+    for pedido in pedidos:
+        print(f"Detalle: {pedido['detalle']}, Pago: {pedido['pago']}, Comprador: {pedido['comprador']}")
+
+# pipeline con $lookup, $unwind y $project
+def pedidos_con_informacion_completa():
+    pipeline = [
+        {
+            "$lookup": {
+                "from": "usuarios",
+                "localField": "comprador",
+                "foreignField": "nombre",
+                "as": "informacion_comprador"
+            }
+        },
+        {
+            "$unwind": "$informacion_comprador"
+        },
+        {
+            "$project": {
+                "_id": 0,
+                "detalle": 1,
+                "pago": 1,
+                "comprador": 1,
+                "correo_comprador": "$informacion_comprador.correo",
+                "tipo_usuario_comprador": "$informacion_comprador.tipo_usuario"
+            }
+        }
+    ]
+    pedidos = coleccion_pedido.aggregate(pipeline)
+    for pedido in pedidos:
+        print(f"Detalle: {pedido['detalle']}, Pago: {pedido['pago']}, Comprador: {pedido['comprador']}, Correo del comprador: {pedido['correo_comprador']}, Tipo de usuario del comprador: {pedido['tipo_usuario_comprador']}")
